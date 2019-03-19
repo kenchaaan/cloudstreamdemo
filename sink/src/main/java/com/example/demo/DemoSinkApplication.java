fee06991f4a27a56d86e87d9c2b407b1e8d752e5
kenchaaan <Nagarjuna2>
2019-03-19 23:45:15 +0900

    int good

--- sink/src/main/java/com/example/demo/DemoSinkApplication.java
+++ sink/src/main/java/com/example/demo/DemoSinkApplication.java
@@ -5,20 +5,27 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.cloud.stream.annotation.StreamListener;
+import org.springframework.cloud.stream.messaging.Processor;
 import org.springframework.cloud.stream.messaging.Sink;
+import org.springframework.integration.annotation.Transformer;
 
 @SpringBootApplication
-@EnableBinding(Sink.class)
+@EnableBinding(Processor.class)
 public class DemoSinkApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoSinkApplication.class, args);
 	}
 
-	@StreamListener(Sink.INPUT)
-	public void print(String tweet){
-		System.out.println("Received " + tweet);
+//	@StreamListener(Sink.INPUT)
+	@Transformer(inputChannel = Processor.INPUT, outputChannel = Processor.OUTPUT)
+	public Object transFormer(String message) {
+		return message.toUpperCase();
 	}
+//	public void print(String tweet){
+//		System.out.println("Received " + tweet);
+//	}
+
 
 	public static class Tweet {
 		public String text;
